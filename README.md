2078b1f134874c0bde12f1cb3ae0d2ba7359c5d3
Bryan Berger <bryan@bryanberger.com>
2017-12-10 23:44:51 -0500

    Init

--- README.md
+++ README.md
@@ -1,73 +1,50 @@
-# palette-contrast-tester
+<div align="center">
+  <img alt="sketch-wcag" src="//" height="" />
+</div>
 
-_This plugin was created using `skpm`. For a detailed explanation on how things work, checkout the [skpm Readme](https://github.com/skpm/skpm/blob/master/README.md)._
+<div align="center">
+  Sketch WCAG: A Sketch plugin that lets you contrast test your entire color palette against the WCAG guidelines all at once.
+</div>
+
+## Installation
+
+##### Recommended
+
+You can install the plugin from [Sketch Runner](http://sketchrunner.com) or [Sketchpacks](https://sketchpacks.com).
+
+<a href="http://bit.ly/SketchRunnerWebsite">
+  <img width="160" height="41" src="http://bit.ly/RunnerBadgeBlue">
+</a>
+<a href="https://sketchpacks.com/bryanberger/sketch-wcag/install">
+  <img width="160" height="41" src="http://sketchpacks-com.s3.amazonaws.com/assets/badges/sketchpacks-badge-install.png">
+</a>
+
+##### Manually
+
+Download [Sketch WCAG](https://github.com/bryanberger/sketch-wcag/archive/master.zip) and double-click the `.sketchplugin` to install.
 
 ## Usage
 
-Install the dependencies
+Select multiple `Shapes` with a fill and run this plugin by pressing `⇧⌘C` (*Shift + Command + C*) or via `Sketch Runner` to preview the WCAG guidelines in your browser. _(Currently: Doesn't work on symbols or groups, doesn't account for opacity)_
 
-```bash
-npm install
-```
+### AA requirements
 
-Once the installation is done, you can run some commands inside the project folder:
+Color contrast of **4.5:1** or **3:1 for large/bold text**
 
-```bash
-npm run build
-```
+### AAA requirements
 
-To watch for changes:
+Color contrast of **7:1** or **4.5:1 for large/bold text**.
 
-```bash
-npm run watch
-```
+## Notes
 
-Additionally, if you wish to run the plugin every time it is built:
+- Large text is defined as 14 point (typically 18.66px) and bold or larger, or 18 point (typically 24px) or larger.
+- aa - greater than 4.5 (for normal sized text)
+- aaLarge - greater than 3 (for 14pt/18px+ bold text || for text larger than 18pt/24px)
+- aaa - greater than 7
+- aaaLarge - greater than 4.5 (for 14pt/18px+ bold text || for text larger than 18pt/24px)
+- WCAG tests are run behind the scenes using [colorable](https://github.com/jxnblk/colorable)
 
-```bash
-npm run start
-```
 
-## Custom Configuration
+## Contributing
 
-### Babel
-
-To customize Babel, you have two options:
-
-* You may create a [`.babelrc`](https://babeljs.io/docs/usage/babelrc) file in your project's root directory. Any settings you define here will overwrite matching config-keys within skpm preset. For example, if you pass a "presets" object, it will replace & reset all Babel presets that skpm defaults to.
-
-* If you'd like to modify or add to the existing Babel config, you must use a `webpack.skpm.config.js` file. Visit the [Webpack](#webpack) section for more info.
-
-### Webpack
-
-To customize webpack create `webpack.skpm.config.js` file which exports function that will change webpack's config.
-
-```js
-/**
- * Function that mutates original webpack config.
- * Supports asynchronous changes when promise is returned.
- *
- * @param {object} config - original webpack config.
- * @param {boolean} isPluginCommand - wether the config is for a plugin command or a resource
- **/
-module.exports = function (config, isPluginCommand) {
-  /** you can change config here **/
-}
-```
-
-## Debugging
-
-To view the output of your `console.log`, you have a few different options:
-
-* Use the [`sketch-dev-tools`](https://github.com/skpm/sketch-dev-tools)
-* Open `Console.app` and look for the sketch logs
-* Look at the `~/Library/Logs/com.bohemiancoding.sketch3/Plugin Output.log` file
-
-Skpm provides a convenient way to do the latter:
-
-```bash
-skpm log
-```
-
-The `-f` option causes `skpm log` to not stop when the end of logs is reached, but rather to wait for additional data to be appended to the input
-
+Contributions are welcomed, [file a pull request](https://github.com/bryanberger/sketch-wcag/issues) or [an issue](https://github.com/bryanberger/sketch-wcag/pulls).
--- src/my-command.js
+++ src/my-command.js
@@ -1,3 +1,176 @@
+const colorable = require('colorable')
+const PATH_TO_CSS = context.plugin.urlForResourceNamed('styles.css').path().replace(/ /g, '\\ ')
+let documentName = ''
+
 export default function(context) {
-  context.document.showMessage("It's alive 🙌")
+  let colors = []
+
+  const sketch    = context.api()
+  const documentObj = context.document
+  const document  = sketch.selectedDocument
+  const selection = document.selectedLayers
+
+  documentName = documentObj.displayName()
+
+  iterator = (layer) => {
+    let layerObj = layer.sketchObject
+
+    if(layer.isShape) {
+      let fillColor = layerObj.style().fills().firstObject().color().NSColorWithColorSpace(nil).hexValue().toString()
+      colors.push("#" + fillColor)
+    }
+  }
+
+  if(selection) {
+    selection.iterate(layer => iterator(layer))
+
+    if(colors.length > 1) {
+      const results = colorable(colors, {compact: false, threshold: 0})
+      const html = createHTML(results)
+      const file = createFile(html)
+
+      openFile(file)
+      context.document.showMessage('HTML file created')
+    } else {
+      context.document.showMessage('Please choose more than 1 color to test')
+    }
+  }
+}
+
+createFile = (htmlStr) => {
+  let htmlContent = NSString.stringWithString_(htmlStr)
+  let filepath = NSTemporaryDirectory() + documentName + '.html'
+  htmlContent.dataUsingEncoding_(NSUTF8StringEncoding).writeToFile_atomically_(filepath, true)
+  let file = NSURL.fileURLWithPath(filepath)
+
+  return file
+}
+
+openFile = (file) => {
+  NSWorkspace.sharedWorkspace().openFile(file.path())
+}
+
+createHTML = (result) => {
+  let markup = table = ''
+  const header = `
+    <style>${readFile(PATH_TO_CSS)}</style>
+    <header>
+      <input class="burger-input" type="checkbox" id="click" />
+      <label for="click">
+        <div class="burger"></div>
+      </label>
+      <nav>
+        <ul>
+          <li><span>AA Large</span> - greater than 3:1 (for 14pt/18px+ bold text || for text larger than 18pt/24px)</li>
+          <li><span>AA</span> - greater than 4.5:1</li>
+          <li><span>AAA Large</span> - greater than 4.5:1 (for 14pt/18px+ bold text || for text larger than 18pt/24px)</li>
+          <li><span>AAA</span> - greater than 7:1</li>
+          <li class="last">Built by: Bryan Berger. <a href="https://github.com/bryanberger/sketch-wcag">Github</a></li>
+        </ul>
+      </nav>
+    </header>
+  `
+
+  for (let i = 0; i < result.length; i++) {
+    let textColor = isDark(result[i].values.rgb) ? '#fff' : '#000'
+    let aaStr = aaLargeStr = aaaStr = aaaLargeStr = ''
+
+    for (let x = 0; x < result[i].combinations.length; x++) {
+      let dict = result[i].combinations[x].accessibility
+      let resultObj = result[i]
+
+      if(!dict['aa'] && !dict['aaLarge'] && !dict['aaa'] && !dict['aaaLarge']) {
+        // all empty for this color skip it
+        continue
+      } else {
+        if(dict['aa']) {
+          aaStr += createColorBadge(resultObj, x)
+        } else {
+          aaStr += createEmptyBadge()
+        }
+
+        if(dict['aaLarge']) {
+          aaLargeStr += createColorBadge(resultObj, x)
+        } else {
+          aaLargeStr += createEmptyBadge()
+        }
+
+        if(dict['aaa']) {
+          aaaStr += createColorBadge(resultObj, x)
+        } else {
+          aaaStr += createEmptyBadge()
+        }
+
+        if(dict['aaaLarge']) {
+          aaaLargeStr += createColorBadge(resultObj, x)
+        } else {
+          aaaLargeStr += createEmptyBadge()
+        }
+      }
+    } // end 2nd loop
+
+    // No Match Check
+    if(aaStr == '' && aaLargeStr == '' && aaaStr == '' && aaaLargeStr == '') {
+      table = `
+        <tr>
+          <td style="text-align: center;">
+            Not accessibile with any of the other colors.
+          </td>
+        </tr>
+        `
+    } else {
+      table = `
+        <tr>
+          <th>AA Large</th>
+          <th>AA</th>
+          <th>AAA Large</th>
+          <th>AAA</th>
+        </tr>
+        <tr>
+          <td>${aaLargeStr}</td>
+          <td>${aaStr}</td>
+          <td>${aaaLargeStr}</td>
+          <td>${aaaStr}</td>
+        </tr>
+      `
+    }
+
+    // Wrap
+    markup += `
+      <div class="strip" style="color: ${textColor}; background-color: ${result[i].hex};"
+        <small>${result[i].hex}</small>
+        <table style="color: ${textColor};">
+          ${table}
+        </table>
+      </div>
+    `
+  } // end first loop
+
+  return header + markup
+}
+
+createColorBadge = (resultObj, x) => {
+  return `
+    <div class="badge" style="color: ${resultObj.hex}; background-color: ${resultObj.combinations[x].hex}">
+      ${resultObj.combinations[x].hex}
+    </div>
+  `
+}
+
+createEmptyBadge = () => {
+  return `
+    <div class="badge">
+      &nbsp;
+    </div>
+  `
+}
+
+readFile = (path) => {
+  return NSString.stringWithContentsOfFile_encoding_error(path, NSUTF8StringEncoding, null)
+}
+
+isDark = (rgb) => {
+	// YIQ equation from http://24ways.org/2010/calculating-color-contrast
+	var yiq = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000
+	return yiq < 128
 }
